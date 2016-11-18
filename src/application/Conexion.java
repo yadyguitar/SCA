6633e9792b1c5f56ed53b66ce5b66bc2b42dfcb7
Yadira Acer <yadyguitar@yahoo.es>
2016-11-17 22:50:25 -0600

    quité archivo derby

--- src/application/Conexion.java
+++ src/application/Conexion.java
@@ -21,33 +21,33 @@ public class Conexion {
     // DATOS PARA CONEXION CON BASE DE DATOS MYSQL
 
     private String USERNAME="root"; //EL MISMO QUE MYSQL
-    private String PASSWORD="301194"; //EL MISMO QUE MYSQL 301194
+    private String PASSWORD="8708278"; //EL MISMO QUE MYSQL 301194
     private String HOST="localhost"; 
     private String PORT="3306";
     private String DATABASE="sca"; //LA DATABASE A USAR
-    private String CLASSNAME="com.mysql.jdbc.Driver";   //EL CONECTOR PARA HACER LA CONEXIÓN
+    private String CLASSNAME="com.mysql.jdbc.Driver";   //EL CONECTOR PARA HACER LA CONEXIï¿½N
     private String URL="jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE+"?user="+USERNAME+"&password="+PASSWORD;
     private Connection conex;
 
     public Conexion(){
         try{
-            //TRATA DE REALIZAR LA CONEXIÓN 
+            //TRATA DE REALIZAR LA CONEXIï¿½N 
             Class.forName(CLASSNAME);
             conex=DriverManager.getConnection(URL, USERNAME,PASSWORD);
         }
         //SI NO PUEDE MANDA ERROR
         catch(ClassNotFoundException | SQLException e){
-            System.err.println("ERROR EN LA CONEXIÓN " +e);
+            System.err.println("ERROR EN LA CONEXIï¿½N " +e);
         }
 
     }
-    //OBTEN LA CONEXIÓN
+    //OBTEN LA CONEXIï¿½N
     public Connection getConexion(){
         return conex;
     }
 
    /* public static void main(String[] args){
-        //CREAR UNA CONEXIÓN
+        //CREAR UNA CONEXIï¿½N
         Conexion conex;
         conex = new Conexion();
         System.out.print("si se pudo");
